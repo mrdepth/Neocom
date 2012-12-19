4e5a1227de6dccab5f22230bca696ca47a13aced
mrdepth <shimanski.artem@gmail.com>
2012-12-19 13:29:02 +0300

    Some xibs cleaned up

--- Classes/en.lproj/prepare.sh
+++ Classes/en.lproj/prepare.sh
@@ -1,8 +1,10 @@
 #/bin/bash
 
+rm xib.strings
 for xib in *.xib
 do
 #strings=`echo $xib | sed -e "s/~ipad//g"`
 strings=`echo $xib | sed -e "s/.xib/.strings/g"`
 ibtool --generate-strings-file $strings $xib
+./stringstool --extract $strings xib.strings
 done

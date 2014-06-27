4150fa00350a6ab4cd7793fed38f2ed041fc01c3
mrdepth <shimanski.artem@gmail.com>
2014-06-27 10:13:22 +0300

    Fixed Cloud Storage crash

--- Neocom/NCAppDelegate.mm
+++ Neocom/NCAppDelegate.mm
@@ -117,6 +117,9 @@
 													 title:NCTaskManagerDefaultTitle
 													 block:^(NCTask *task) {
 														 NCStorage* storage = [NCStorage cloudStorage];
+														 if (!storage)
+															 storage = [NCStorage fallbackStorage];
+
 														 [NCStorage setSharedStorage:storage];
 														 NCAccountsManager* accountsManager = [[NCAccountsManager alloc] initWithStorage:storage];
 														 [NCAccountsManager setSharedManager:accountsManager];
@@ -260,6 +263,8 @@
 													 title:NCTaskManagerDefaultTitle
 													 block:^(NCTask *task) {
 														 storage = [NCStorage cloudStorage];
+														 if (!storage)
+															 storage = [NCStorage fallbackStorage];
 														 accountsManager = [[NCAccountsManager alloc] initWithStorage:storage];
 													 }
 										 completionHandler:^(NCTask *task) {

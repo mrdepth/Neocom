fdbd02fceee221b474c2b46274c0a0977e935511
Shimanski Artem <shimanski.artem@gmail.com>
2018-01-29 09:07:39 +0300

    1.3.0

--- Neocom/Neocom/Constants.swift
+++ Neocom/Neocom/Constants.swift
@@ -216,7 +216,7 @@ enum InAppProductID: String {
 	var period: String {
 		switch self {
 		case .removeAdsMonth:
-			return NSLocalizedString("1 Month", comment: "")
+			return NSLocalizedString("per month", comment: "")
 		}
 	}
 	
